270527edb2f5faea44ef4d3c3f654bbd5f0ae008
Alonso Montero <alon182@gmail.com>
2016-09-28 16:35:53 -0600

    Added code for Right Antenna

---
+++ RightAntenna.py
@@ -0,0 +1,28 @@
+
+
+#!/usr/bin/python
+
+"""
+LeftAntenna.py
+-------------
+Controls the left antenna by moving it to a position
+between 0(0) and 1(180) 
+
+"""
+# Author: Alonso Montero <alon182@gmail.com
+# License:  GPL v2
+
+from Servo import *
+from conf import *
+
+class LeftAntenna():
+    def __init__(self)
+        self.configuration = conf("Setting.ini")
+        self.channel = self.configuration.getSetting("LeftAntenna","channel")
+        self.frequency = self.configuration.getSetting("LeftAntenna","frequency")
+        self.minPulseLength = self.configuration.getSetting("LeftAntenna","minPulseLength")
+        self.maxPulseLength = self.configuration.getSetting("LeftAntenna","maxPulseLength")
+        self.servo = Servo(self.channel, self.frequency, self.minPulseLength, self.maxPulseLength)
+
+    def move(positions):
+        self.servo.move(positions)
