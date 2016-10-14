66ffe19562af30f7e0c7e0de1b77f7fb9582631c
Alonso Montero <alon182@gmail.com>
2016-10-14 11:56:47 -0600

    first commit for cli

---
+++ cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+cli.py
+-------------
+Higher level implementation of parts movement via command line.
+"""
+# Author: Alonso Montero <alon182@gmail.com
+# License:  GPL v2
+
+def main():
+    pass
+@click.command()
+@click.option("-move", type=click.STRING, default= None, help="Move a specific part of the head.")
+
